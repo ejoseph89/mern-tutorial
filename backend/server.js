288f133dde21bd15b6e932a0e51ae7df7c9c2a17
Emil Joseph <emiljoseph89@gmail.com>
2022-05-02 23:37:20 -0600

    set up initial REST api for goals

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 // Packages
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+
+// Connect database
+connectDB();
 
 // Initializing express
 const app = express();
@@ -20,5 +25,5 @@ app.use(errorHandler);
 
 // Server listening
 app.listen(port, () => {
-  console.log(`SERVER STARTED ON PORT ${port}`);
+  console.log(`SERVER STARTED ON PORT ${port}`.cyan.bold);
 });
